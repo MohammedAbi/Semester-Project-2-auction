7c5aa7512bfc22d2794eeb627e8abc525632497e
Mohammed Abdul Abi <mohammedabdulabi@gmail.com>
2025-03-02 15:47:36 +0100

    Added redirect

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/src",
+  base: "/",
   build: {
     outDir: "dist",
   },
